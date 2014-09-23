0ea46c59a0d25f9c9a5005862aa993df60d39386
Chael Kruip <chael.kruip@quintel.com>
2014-09-23 15:21:35 +0200

    Scaled the NL demand curve to incorporate export to BE, NOR and GBR

--- tmp/convergence/sum_demand_curves.py
+++ tmp/convergence/sum_demand_curves.py
@@ -10,18 +10,24 @@ x_plots = 1
 y_plots = 1
 
 # load curves
-nl2013 = np.genfromtxt("/Users/kruip/Projects/etengine/tmp/convergence/20130923_1502/NL_373837_2014-09-23_15-01-42/demand.csv", delimiter = ',')
+filepath = "/Users/kruip/Projects/etengine/tmp/convergence/20130923_1502/NL_373837_2014-09-23_15-01-42/demand.csv"
+nl2013 = np.genfromtxt(filepath, delimiter = ',')
 
-#ofile = open("/Users/kruip/Projects/merit-convergence/data/nl/interconnector_load_curves/NOR_NL_2023.csv","w")
-#for i in range(0,len(nor2013)):
-#    if i > 7234 and i < 8481:
-#        ofile.write("700\n")
-#    else:
-#        ofile.write(""+str(nor2013[i])+"\n")
-#
-#ofile.close()
+total = np.sum(nl2013)/1e6
+print "Total demand in NL: ", total, " TWh = ", total * 3.6, " PJ"
 
-year = 8760
+old_total = total * 3.6 * 1e9
+new_total = 465752837814.0096
+
+scaling_factor = new_total / old_total
+print "Scaling factor: ", scaling_factor
+
+# Overwriting the original file!
+ofile = open(filepath,"w")
+for i in nl2013:
+    ofile.write(""+str(i*scaling_factor)+"\n")
+
+ofile.close()
 
 plt.title("Demand for electricity")
 plt.subplot(y_plots, x_plots, 1)
@@ -32,5 +38,3 @@ plt.ylabel('MW')
 plt.xlabel('hours')
 plt.show()
 
-total = np.sum(nl2013)/1e6
-print "Total demand in NL: ", total, " TWh = ", total * 3.6, " PJ"
