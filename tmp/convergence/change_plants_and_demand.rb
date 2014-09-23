afd4dc965114b0b2f8454ec04382411128f4cac0
Chael Kruip <chael.kruip@quintel.com>
2014-09-23 09:35:07 +0200

    Fixed script. Now it writes the changes to the NL archive info file

--- tmp/convergence/change_plants_and_demand.rb
+++ tmp/convergence/change_plants_and_demand.rb
@@ -45,5 +45,9 @@ end
 # Change demand (add export from 2013 (54 PJ) to the FD + OU + loss)
 demand_file = YAML.load_file("archive-info.yml")
 
-if demand_file[:area] = 'nl':
+if demand_file[:area] == 'nl'
+  puts "Changing demand"
   demand_file[:total_demand] = demand_file[:total_demand] + 54.0e9
+end
+
+File.write("archive-info.yml", YAML.dump(demand_file))
